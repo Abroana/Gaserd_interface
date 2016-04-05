f0b144f1363108d7c7bb4044420821059d5a9fe7
Anastasia Abrosimova <abrosimova.ana@yandex.ru>
2016-04-05 22:41:45 +0300

    Created editor window

--- js/loader-animation.js
+++ js/loader-animation.js
@@ -7,7 +7,10 @@ var Loader = {
     var parent = this, div = '';
     div = '<div class="loader-container">';
       div += '<div class="loader-container__title">';
-        
+        div += '<div class="title__circle title__circle-red"></div>'
+        div += '<div class="title__circle title__circle-yellow"></div>'
+        div += '<div class="title__circle title__circle-green"></div>'
+        div += '<span class="title__header">Gaserd</span>'
       div += '</div>';
       div += '<div class="loader-container__body">';
         div += text;
